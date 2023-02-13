99221a4f083c1b816be65d41429b0f4299f4a63c
Payel <payel.nandy2580@gmail.com>
2023-02-13 23:29:33 +0530

    linked list

--- linkedlist.py
+++ linkedlist.py
@@ -77,6 +77,38 @@ class linked_list:
             itr = itr.next
             count += 1
 
+    def insert_after_value(self,data_after,data_to_insert):
+
+        if self.head is None:
+            return
+        if self.head == data_after:
+            self.head.next = Node(data_to_insert,self.head.next)
+            return
+        itr = self.head
+        while itr:
+            if itr.data == data_after:
+                itr.next = Node(data_to_insert,itr.next)
+                break
+            itr = itr.next
+
+
+    def remove_by_value(self,data):
+        if self.head is None:
+            return
+        if self.head.data == data:
+            self.head == self.head.next
+            return
+        itr = self.head
+        while itr.next:
+            if itr.next.data == data:
+                itr.next = itr.next.next
+                break
+            itr = itr.next
+
+
+
+
+
 
 
 
@@ -93,6 +125,11 @@ if __name__=='__main__':
     ll.print()
     ll.insert_at(0,'figs')
     ll.insert_at(2,'grapes')
+    ll.print()
+    ll.insert_after_value('mangoes','pineapple')
+    ll.print()
+
+    ll.remove_by_value('grapes')
     ll.print()
 
 
